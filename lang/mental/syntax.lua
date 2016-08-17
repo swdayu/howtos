6ac3e8992b11c75b40a9aa6bafc519128740dc6d
massivesupernova <flw_r@163.com>
2016-08-18 02:05:35 +0800

    Update syntax.lua

--- lang/mental/syntax.lua
+++ lang/mental/syntax.lua
@@ -3,7 +3,7 @@
 local lpeg = require "lpeg"
 
 function lpeg.St(t)
-  local patt = lpeg.P("")
+  local patt = lpeg.P""
   for _,v in ipairs(t) do
     patt = patt + lpeg.P(v)
   end
@@ -11,7 +11,7 @@ function lpeg.St(t)
 end
 
 function lpeg.Cst(t)
-  local patt = lpeg.P("")
+  local patt = lpeg.P""
   for i,v in ipairs(t) do
     patt = patt + lpeg.P(v) * lpeg.Cc(i)
   end
@@ -79,7 +79,7 @@ local function l_keyword()
 end
 
 local function l_space()
-  return S("\x20\x09\x0B\x0C")^1 / f_space
+  return S"\x20\x09\x0B\x0C"^1 / f_space
 end
 
 local function l_newline()
@@ -87,7 +87,7 @@ local function l_newline()
 end
 
 local function l_comment()
-  return (P("//") * (1-St(eof))^0 * St(eof)) / f_comment
+  return (P"//" * (1-St(eof))^0 * St(eof)) / f_comment
 end
 
 local function f_matchtime(deststr, pos, val)
@@ -100,17 +100,17 @@ local function f_matchtime(deststr, pos, val)
 end
 
 local function f_blockcomment(stars, content)
-  token(BLOCKCOMMENT, starts, content)
+  token(BLOCKCOMMENT, stars, content)
   if content == "/" then
-    return "/" .. starts .. "/"
+    return "/" .. stars .. "/"
   end
-  return "/" .. starts .. content .. starts .. "/"
+  return "/" .. stars .. content .. stars .. "/"
 end
 
 local function l_blockcomment()
   local start = Cg(P"/" * C(P"*"^1), "commentstart")
-  local tail = Cb("commentstart") / "%1/"
-  return (start * (Cb("commentstart")/1) * (C"/" + C((1-Cmt(tail, f_matchtime))^1) * Cmt(tail, f_matchtime))) / f_blockcomment
+  local tail = Cb"commentstart" / "%1/"
+  return (start * (Cb"commentstart"/1) * (C"/" + C((1-Cmt(tail, f_matchtime))^1) * Cmt(tail, f_matchtime))) / f_blockcomment
 end
 
 ```
